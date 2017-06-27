bea36d4a7d255ce45addc2e47ab2ea4c0231b9fa
tushargr <tushar.gurjar1999@gmail.com>
2017-06-27 05:31:22 +0530

    Add files via upload

--- Gameai.py
+++ Gameai.py
@@ -8,9 +8,9 @@ import math
 input_nodes=102
 hidden_nodes=150
 output_nodes=1
-alpha=0.5
+alpha=0.1
 lmbda=0.5
-gamma=0.8
+gamma=0.5
 
 def TDlambda(list_rewards):
     length=len(list_rewards)
@@ -18,7 +18,8 @@ def TDlambda(list_rewards):
         if(i!=length-1):
             list_rewards[i]=(1-lmbda)*list_rewards[i]
         list_rewards[i]=list_rewards[i]*math.pow(lmbda,i)
-    a=[sum(list_rewards)]
+    a=sum(list_rewards)
+
     return a
 def computer_place_ships(board,ships):
     for ship in ships.keys():
@@ -34,24 +35,7 @@ def computer_place_ships(board,ships):
             valid=validate(board,ships[ship],x,y,ori)
         board=place_ship(board,ships[ship],ship[0],ori,x,y)
     return board
-'''
-def computer_place_ships(board,ships):
-    for ship in ships.keys():
-        #genreate random coordinates and vlidate the postion
-		valid=False
-		while(not valid):
-            x=(random.randint(1,10)-1)
-            #y=(random.randint(1,10)-1)
-            o=random.randint(0,1)
-            if(o == 0):
-                ori = "v"
-            else:
-                ori = "h"
-            valid = validate(board,ships[ship],x,y,ori)
-        #place the ship
-        board = place_ship(board,ships[ship],ship[0],ori,x,y)
-    return board
-'''
+
 def place_ship(board,ship,s,ori,x,y):
     #place ship based on orientation
     if ori == "v":
@@ -85,12 +69,12 @@ def validate(board,ship,x,y,ori):
 input_layer=tf.placeholder(tf.float32,[None,input_nodes])
 Q_s_a=tf.placeholder(tf.float32,[None,output_nodes])
 
-weight1=tf.Variable(tf.random_normal(shape=(input_nodes,hidden_nodes)))
+weights1=tf.Variable(tf.random_normal(shape=(input_nodes,hidden_nodes)))
 baises1=tf.Variable(tf.zeros(shape=(hidden_nodes)))
 weights2=tf.Variable(tf.random_normal(shape=(hidden_nodes,output_nodes)))
 baises2 =tf.Variable(tf.zeros(shape=(output_nodes)))
 
-z1=tf.matmul(input_layer,weight1)+baises1
+z1=tf.matmul(input_layer,weights1)+baises1
 a1=tf.nn.relu(z1)
 
 pQ_s_a=tf.matmul(a1,weights2)+baises2
@@ -102,11 +86,11 @@ game_progress=[]
 init=tf.initialize_all_variables()
 with tf.Session() as session:
     session.run(init)
-
     for game in range(1,10001):
         print game
         k=0
         states=[]
+
         actions=[]
         state=np.zeros(shape=(102),dtype=np.float32)
         hitlog=[]
@@ -138,6 +122,7 @@ with tf.Session() as session:
                     dict={}
                     dict[input_layer]=stte
                     pQsa=session.run(pQ_s_a,feed_dict=dict)
+
                     if(flag==1):
                         maxQ=pQsa
                         actionindex[0]=i
@@ -147,9 +132,9 @@ with tf.Session() as session:
                         maxQ=pQsa
                         actionindex[0]=i
                         actionindex[1]=j
-            pQ_s_alist.append(maxQ)
-            state[100:102]=actionindex
-            states.append(state)
+            pQ_s_alist.extend(maxQ)
+            state[100:102]=copy.deepcopy(actionindex)
+            states.append(copy.deepcopy(state))
             actions.append(actionindex)
             if(board[actionindex[0]-1][actionindex[1]-1]==-1):
                 h=-1
@@ -172,8 +157,7 @@ with tf.Session() as session:
                 longreward=0
                 while(t<=stp):
                     if(t==stp and stp<len(states)):
-                        longreward+=math.pow(gamma,p)*pQ_s_alist[t]
-
+                        longreward+=math.pow(gamma,p)*pQ_s_alist[t][0]
                     elif(t<len(states)):
                         longreward+=math.pow(gamma,p)*hitlog[t]
                     p+=1
@@ -181,19 +165,32 @@ with tf.Session() as session:
                 list_rewards.append(longreward)
                 stp+=1
             aQ_s_a.append(TDlambda(list_rewards))
-        game_progress.append(len(states))
-        per=np.random.permutation(len(states))
 
-        perstates=[states[i] for i in per]
-        peraQ_s_a=[aQ_s_a[i] for i in per]
+        state_action=np.zeros((len(states),102),dtype=np.float32)
+        Q=np.zeros((len(states),1),dtype=np.float32)
+        for i in range(0,len(states)):
+            state_action[i,:]=copy.deepcopy(states[i])
+            Q[i,0]=aQ_s_a[i]
+
+        game_progress.append(len(states))
+
+        per=np.random.permutation(len(states))
+        state_action=state_action[per,:]
+        Q=Q[per,:]
+
         steps=10
         for step in range(1,steps+1):
-
-            feed_dict={}
-            feed_dict[input_layer]=perstates
-            feed_dict[Q_s_a]=peraQ_s_a
-            _,l=session.run([optimizer,cost],feed_dict=feed_dict)
-        print '-----',game_progress[game-1]
+            for i in range(0,len(states)):
+                inpt=np.zeros((1,102),dtype=np.float32)
+                outpt=np.zeros((1,1),dtype=np.float32)
+                inpt[0,:]=state_action[i,:]
+                outpt[0,0]=Q[i,0]
+                feed_dict={}
+                feed_dict[input_layer]=inpt
+                feed_dict[Q_s_a]=outpt
+                _,l=session.run([optimizer,cost],feed_dict=feed_dict)
+        print session.run(weights1),session.run(weights2)
+        print '-----',game_progress[game-1],'-----',sum(game_progress)*1.0/len(game_progress)
     w1=session.run(weights1)
     b1=session.run(baises1)
     w2=session.run(weights2)
